76ff8127d2e3c52d1aeaa8c8b6ae9dcdcf0e75dd
Amar Kumar <kumaramar3550amar@gmail.com>
2026-05-27 23:15:18 +0530

    Add files via upload

---
+++ EMP-WITH-DATABASE.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EMP-WITH-DATABASE.aspx.cs" Inherits="EMPL_1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Emp no&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Address&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Salary&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" Height="190px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="388px"></asp:ListBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="SAVE" OnClick="Button1_Click"  />
+            <asp:Button ID="Button2" runat="server" Text="UPDATE" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="DEL" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" runat="server" Text="DISP" OnClick="Button4_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ EMP-WITH-DATABASE.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace EMPL_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con=new SqlConnection();
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["slow"].ConnectionString
+            );
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insert into [dbo].[Table] values(@eno,@ename,@add,@sal)";
+            cmd.Connection = con;
+
+            cmd.Parameters.AddWithValue("@eno", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@ename", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@add", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@sal", TextBox4.Text);
+
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            Disp_rec();
+
+            cursor_rec();
+        }
+
+        private void cursor_rec()
+        {
+            TextBox1.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+            TextBox4.Text = string.Empty;
+            TextBox1.Focus();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Disp_rec ();
+
+        }
+
+        private void Disp_rec()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from [dbo].[Table]";
+            cmd.Connection = con;
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            ListBox1.DataTextField = "Name";
+            ListBox1.DataValueField = "Emp no.";
+            ListBox1.DataSource = dr;
+            ListBox1.DataBind();
+            dr.Close();
+            cmd.Dispose();
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from [dbo].[Table] where [Emp no.]=@eno ";
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@eno", ListBox1.SelectedValue);
+            SqlDataReader dr=cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                TextBox1.Text = dr["Emp no."].ToString();
+                TextBox2.Text = dr["Name"].ToString();
+                TextBox3.Text = dr["Address"].ToString() ;
+                TextBox4.Text = dr["Salary"].ToString();
+            }
+            dr.Close ();
+            cmd.Dispose();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "update [dbo].[Table] set Name=@en,Address=@add,Salary=@sal where [Emp no.]=@eno ";
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@en", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@add", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@sal",TextBox4.Text);
+            cmd.Parameters.AddWithValue("@eno", TextBox1.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose ();
+            cursor_rec();
+            Disp_rec();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "delete from [dbo].[Table] where [Emp no.]=@eno";
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@eno", TextBox1.Text);
+            cmd.ExecuteNonQuery ();
+            cmd.Dispose ();
+            Disp_rec();
+            cursor_rec();
+        }
+    }
+}
